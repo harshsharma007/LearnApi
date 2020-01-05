103e45ab4d416e0492d5c12361c4c7c5d3e1fe6c
Harsh Sharma <harshsharmahs@gmail.com>
2020-01-05 18:17:14 +0530

    -Second commit

---
+++ LearnApi/Controllers/UsingHttpVerbsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LearnApi.Controllers
+{
+    //Web Api controller characteristics
+    //1. It must be derived from System.Web.Http.ApiController class.
+    //2. It can be created under any folder in the project's root folder. However, it is recommended to create controller classes in the Controllers folder
+    //as per the convention.
+    //3. Action method name can be the same as HTTP verb name or it can start with HTTP verb with any suffix (case in-sensitive) or you can apply HTTP verb
+    //attributes to method.
+    //4. Return type of an action method can be any primitive or complex type.
+
+    public class UsingHttpVerbsController : ApiController
+    {
+        [HttpGet]
+        public List<string> Values()
+        {
+            return new List<string>() { "Harsh", "Sharma" };
+        }
+
+        [HttpGet]
+        public string Value(int id)
+        {
+            return "Harsh";
+        }
+
+        [HttpPost]
+        public void SaveNewValues([FromBody]string Value)
+        {
+            //Logic
+        }
+
+        [HttpPut]
+        public void UpdateValue(int id, [FromBody]string Value)
+        {
+            //Logic
+        }
+
+        [HttpDelete]
+        public void RemoveValue(int id)
+        {
+            //Logic
+        }
+    }
+    //Naming convention of a web method.
+    
+    //HTTP Method           Possible Web Api Action Method Name             Usage
+    //GET                   Get()                                           Retrieves Data
+    //                      get()
+    //                      GET()
+    //                      GetAllStudents()
+    //                      *any name starting with Get*
+    
+    //POST                  Post()                                          Inserts new record
+    //                      post()
+    //                      POST()
+    //                      PostNewStudent()
+    //                      *any name starting with Post*
+
+    //PUT                   Put()                                           Updates existing record
+    //                      put()
+    //                      PUT()
+    //                      PutStudent()
+    //                      *any name starting with Put*
+
+    //PATCH                 Patch()                                         Updates record partially
+    //                      patch()
+    //                      PATCH()
+    //                      PatchStudent()
+    //                      *any name starting with Patch*
+
+    //DELETE                Delete()                                        Deletes record
+    //                      delete()
+    //                      DELETE()
+    //                      DeleteStudent()
+    //                      *any name starting with Delete*
+}
--- LearnApi/Controllers/WebapiVsWcfController.cs
+++ LearnApi/Controllers/WebapiVsWcfController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 
 namespace LearnApi.Controllers
 {
+    //Full form of REST is Representational State Transfer
+
     //Web Api Characteristics
     //1. Web Api is an ideal platform for building RESTful services.
     //2. Web Api is built on top of ASP.Net and supports ASP.Net request/response pipeline.
@@ -20,11 +22,77 @@ namespace LearnApi.Controllers
     //Web API Version           Supported.NET Framework         Coincides with      Supported in
     //Web API 1.0	            .NET Framework 4.0	            ASP.NET MVC 4	    VS 2010
     //Web API 2 - Current       .NET Framework 4.5	            ASP.NET MVC 5	    VS 2012, 2013
-    public class WebapiVsWcfController : ApiController
-    {
-        public void Post([FromBody]string value)
-        {
 
+    public class Name
+    {
+        public string value { get; set; }
+    }
+
+    public class WebapiVsWcfController : ApiController //Without using HTTP verbs.
+    {
+        //Web api vs Wcf
+        //Web api                                                       Wcf
+        //Open source and ships with .Net framework                     Ships with .Net framework.
+        //Supports only HTTP protocol                                   Supports HTTP, TCP, UDP and custom transport protocol.
+        //Maps http verbs to methods                                    Uses attributes based programming model.
+        //Uses routing and controller concept similar to MVC            Uses Service, Operation and Data Contracts.
+        //Does not support Reliable Messaging and transaction           Supports Reliable Messaging and Transactions.
+        //Web api can be configured using HTTPConfiguration class       Uses web.config and attributes to configure a service.
+        //but not in web.config
+        //Ideal for building RESTful services                           Supports RESTful services with limitations.
+
+        public void Post([FromBody]string name)
+        {
+            //To pass value to this method, the type selected must be "post" and in postman body "raw" should be selected as an option and the request must be
+            //send in JSON format only.
+            //Sample format of request
+            //"Harsh" -> this is the way to assign value to parameter name
+        }
+
+        //When to choose Web Api
+        //If you are using .Net framework 4.0 or above.
+        //If you want to build a service that supports only HTTP Protocol.
+        //If you want to build RESTful HTTP based services.
+        //If you are familiar with ASP.NET MVC.
+
+        public void Get(Name name)
+        {
+            //To pass value to this method, the type selected must be "Get" and in postman body "raw" should be selected as an option and the request must be
+            //send in JSON format only.
+            //Sample format of request
+            //{
+            //  "value" : "Harsh"
+            //}
+            
+            //When it comes to sending a value to an object, then the name of the property must be assigned with a value.
+            //As in above example
+            //"value" is used, not the variable "name".
+        }
+
+        public void Put(int id, [FromBody] string value)
+        {
+            //To pass value to this method, the type selected must be "Put" and in postman body "raw" should be selected as an option and the request must be
+            //send in JSON format only.
+            //Sample format of request
+            //Url: http://localhost:64808/api/WebapiVsWcf/44
+            //"Harsh" -> will be written in body.
+        }
+
+        public void Delete(int id)
+        {
+            //To pass value to this method, the type selected must be "Delete".
+            //Sample format of request
+            //Url: http://localhost:64808/api/WebapiVsWcf/44
         }
     }
+
+    //Difference between Web Api and MVC Controller
+    //Web Api Controller                                                    MVC Controller
+    //Derives from System.Web.Http.ApiController class                      Derives from System.Web.Mvc.Controller class.
+    //Method name must start with Http verbs otherwise                      Must apply appropriate Http verbs attribute.
+    //apply http verbs attribute.
+    //Specialized in returning data.                                        Specialized in rendering view.
+    //Return data automatically formatted based on Accept-Type              Returns ActionResult or any derived type.
+    //header attribute. Default to json or xml.
+    //Requires .Net 4.0 or above.                                           Requires .Net 3.5 or above.
 }
