6a64b0693c2c9fbcd7d0a04aff26092ec2dfb44c
Harsh Sharma <harshsharmahs@gmail.com>
2020-01-08 00:37:17 +0530

    Initial

---
+++ LearnApi/Controllers/ConfigureWebApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LearnApi.Controllers
+{
+    //Web Api supports code based configuration. It can not be configured in web.config file. We can configure Web Api to customize the behavior of Web Api
+    //hosting infrastructure and components such as routes, formatters, filters, DependencyResolver, MessageHandlers, ParameterBindingRules, Properties,
+    //services etc
+
+    public class ConfigureWebApiController : ApiController
+    {
+        //Web Api configuration process starts when the application starts. It calls GlobalConfiguration.Configure(WebApiConfig.Register) in the
+        //Application_Start method. The Configure() method requires the callback method where Web Api has been configured in code. By default this is the
+        //static WebApiConfig.Register() method.
+
+        //As you can see in WebApiConfig.cs, WebApiConfig.Register() method includes a parameter of HttpConfiguration type which is then used to configure the
+        //Web Api. The HttpConfiguration is the main class which includes following properties using which you can override the default behavior of Web Api.
+
+        //Property                      Description
+        //DependencyResolver            Gets or sets the dependency resolver for dependency injection.
+        //Filters                       Gets or sets the filters.
+        //Formatters                    Gets or sets the media-type formatters.
+        //IncludeErrorDetailPolicy      Gets or sets a value indicating whether error details should be included in error messages.
+        //MessageHandlers               Gets or sets the message handlers.
+        //ParameterBindingRules         Gets the collection of rules for how parameters should be bound.
+        //Properties                    Gets the properties associated with this Web Api instance.
+        //Routes                        Gets the collection of routes configured for the Web Api.
+        //Services                      Gets the Web Api services.
+
+        //What is a callback method?
+        //A callback is a function that will be called when a process is done executing a specific task. The usage of a callback is usually in asynchronous logic.
+        //To create a callback in C#, you need to store a function address inside a variable. This is achieved using a delegate or the new lambda semantic Func or
+        //Action.
+    }
+}
---
+++ LearnApi/Controllers/WebApiRoutingController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+//Routing uses the namespace -> System.Web.Http.Routing
+namespace LearnApi.Controllers
+{
+    //Web Api routing is similar to Asp.Net MVC routing. It routes an incoming HTTP request to a particular action method on a Web Api controller.
+    //Web Api supports two types of routing:
+    //1. Convention-based routing.
+    //2. Attribute routing.
+
+    public class WebApiRoutingController : ApiController
+    {
+        //Convention-based routing
+        //In the convention-based routing, Web Api uses route templates to determine which controller and action method to execute. At least one route template
+        //must be added into route table in order to handle various HTTP requests.
+        //When we create a project, it creates WebApiConfig class in the App_Start folder with default route as in the WebApiConfig.cs class.
+
+        //public static class WebApiConfig
+        //{
+        //    public static void Register(HttpConfiguration config)
+        //    {
+        //        // Enable attribute routing
+        //        config.MapHttpAttributeRoutes();
+
+        //        // Add default route using convention-based routing
+        //        config.Routes.MapHttpRoute(
+        //            name: "DefaultApi",
+        //            routeTemplate: "api/{controller}/{id}",
+        //            defaults: new { id = RouteParameter.Optional }
+        //        );
+        //    }
+        //}
+
+        //In the above WebApiConfig.Register() method, config.MapHttpAttributeRoutes() enables attribute routing. The config.Routes is a route table or route
+        //collection of type HttpRouteCollection. The "DefaultApi" route is added in the route table using MapHttpRoute() extension method. The MapHttpRoute()
+        //extension method internally creates a new instance of IHttpRoute and adds it to an HttpRouteCollection. However, you can create a new route and add
+        //it into a collection manually as shown below.
+
+        public static class WebApiConfig
+        {
+            public static void Register(HttpConfiguration config)
+            {
+                config.MapHttpAttributeRoutes();
+
+                //Define route
+                IHttpRoute defaultRoute = config.Routes.CreateRoute("api/{controller}/{id}", new { id = RouteParameter.Optional }, null);
+
+                //Add route
+                config.Routes.Add("DefaultApi", defaultRoute);
+            }
+        }
+
+        //The following table lists parameters of MapHttpRoute method.
+        //Parameter             Description
+        //name                  Name of the route
+        //routeTemplate         URL pattern of the route
+        //defaults              An object parameter that includes default route values
+        //constraints           Regex expression to specify characteristic of route values
+        //handler               The handler to which the request will be dispatched
+    }
+}
