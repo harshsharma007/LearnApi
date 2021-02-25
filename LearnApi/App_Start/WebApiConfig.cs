d37f8035f40eb9aabee80e6fe2ddb821ecf050a2
Harsh Sharma <harshsharmahs@gmail.com>
2021-02-25 23:10:50 +0530

    Initial Commit

--- LearnApi/App_Start/WebApiConfig.cs
+++ LearnApi/App_Start/WebApiConfig.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-<<<<<<< HEAD
-=======
 using LearnApi.Controllers.ExceptionHandling;
->>>>>>> 5f425d3 (Learn Api)
 
 namespace LearnApi
 {
@@ -14,21 +11,15 @@ namespace LearnApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-<<<<<<< HEAD
-=======
             config.Filters.Add(new CustomExceptionFilter());
->>>>>>> 5f425d3 (Learn Api)
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-<<<<<<< HEAD
                 routeTemplate: "api/{controller}/{action}/{id}",
-=======
                 routeTemplate: "api/{controller}/{id}",
->>>>>>> 5f425d3 (Learn Api)
                 defaults: new { id = RouteParameter.Optional }
             );
         }
